494038106d25df85a2b211234d6696998cdd92b3
sqlwwx <wwx@wuweixing.com>
2024-06-17 20:30:24 +0800

    feat: json query editor

---
+++ packages/bruno-app/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Modal } from 'components/Modal';
+export { default as CodeEditor } from 'components/CodeEditor';
+export { default as CodeEditorBar } from 'components/CodeEditorBar';
+export { default as SingleLineEditor } from 'components/SingleLineEditor';
